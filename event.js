600a2f6e08f6a3ef0b9aa4d77cf21c2e32bd8f0d
Amber <957890374@qq.comr>
2017-07-09 17:34:38 +0800

    add event

---
+++ event.js
@@ -0,0 +1,28 @@
+var events = require('events')
+var eventEmitter = new events.EventEmitter()
+function listener1(){
+	console.log('listener1')
+}
+function listener2(){
+	console.log('listener2')
+}
+
+eventEmitter.addListener('connection',listener1)
+
+eventEmitter.on('connection',listener2)
+
+let connectCount = eventEmitter.listenerCount('connection')
+
+console.log(`connection events count is ${connectCount}`)
+
+eventEmitter.emit('connection')
+
+eventEmitter.removeListener('connection', listener1)
+
+console.log('removeListener1')
+
+eventEmitter.emit('connection')
+
+connectCount = eventEmitter.listenerCount('connection')
+
+console.log(`connection events count is ${connectCount}`)
